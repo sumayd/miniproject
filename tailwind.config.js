d101148dd7710907054f75f537e9d566390dc1c7
sumayd <sumeyyeaydemir@gmail.com>
2022-01-06 17:37:03 +0300

    Container + Breakpoints + Debug Screen Plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    debugScreens: {
+      position: ['top', 'left'],
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-debug-screens'),
+  ],
 }
